651c5fcc0ec75e11e233a8c070cc328db764e5c5
USER <kellylan322@gmail.com>
2022-09-14 10:31:09 +0800

    Introducing constants

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello FJU!");
+      int x;
+      x = 5;
+      x = 10;
+      x = x + 1;
+      x += 1;
+        System.out.println(x);
     }
 }
