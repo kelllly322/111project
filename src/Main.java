5b0c3db4e3f66443cfc22d401f617adb8d6b7ee4
USER <kellylan322@gmail.com>
2022-09-14 11:42:18 +0800

    String practice

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-     int x; // declare
-     x = 10; // assignment
-     System.out.println(x);
+    String name = "FJU Catholic University";
+        System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
 
-     int y = 20; // declare and assignment
-     System.out.println(y);
+        //INDEX 0,1,2,3,......,n=1; n=length()
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(3));
+        System.out.println(name.charAt(name.length()-1));
+        System.out.println(name.indexOf('i'));
+        System.out.println(name.indexOf("Uni"));
 
-     var z = 50; //var  means
-     System.out.println(z);
-
-     int i, j ,k; // declare multiple constants
+        String age = "25";
+        System.out.println(Integer.parseInt(age)); //Java build-in method
     }
 }
